e285b7319448d9c02b7ac70a018fef6347af3e1e
Maxime Guilbert <mx.guilbert@gmail.com>
2026-02-08 08:25:58 -0500

    data: Update hall of fame

--- src/views/HallOfFameView.vue
+++ src/views/HallOfFameView.vue
@@ -29,8 +29,25 @@ onBeforeUnmount(() => {
   <section class="logo">
     <Logo></Logo>
   </section>
-  <section style="text-align: center; padding-top: 40px; min-height: 48vh;">
-    <span style="font-size: 3em;">{{  t("hof.waiting_members") }}</span>
+  <section style="min-height: 48vh; padding: 40px 20% 0px 20%;">
+    <h2>{{  t("hof.badge") }}</h2>
+    <h3>{{  t("hof.badge_roche") }}</h3>
+    <h3>{{  t("hof.badge_cascade") }}</h3>
+    <ul>
+      <li><b>Redfire</b> : 1/3</li>
+    </ul>
+    <h3>{{  t("hof.badge_foudre") }}</h3>
+    <h3>{{  t("hof.badge_prisme") }}</h3>
+    <h3>{{  t("hof.badge_ame") }}</h3>
+    <h3>{{  t("hof.badge_marais") }}</h3>
+    <h3>{{  t("hof.badge_volcan") }}</h3>
+    <h3>{{  t("hof.badge_terre") }}</h3>
+    <hr/>
+    <h2>{{  t("hof.contest") }}</h2>
+    <ul>
+      <li><b>Redfire</b> : 7</li>
+      <li><b>Lollix_D_Nenzo</b> : 4</li>
+    </ul>
   </section>
 </template>
 
@@ -39,6 +56,18 @@ onBeforeUnmount(() => {
   display: flex;
   justify-content: center;
   height: 220px;
+}
+
+section {
+  background-color: #1a2344;
+}
+
+h2 {
+  margin-left: 40px;
+}
+
+h3 {
+  margin-left: 80px;
 }
 
 </style>
